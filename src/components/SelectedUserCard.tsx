61af8894b0ab6664872c46026fa0f49b82f32aa4
PaneruVipin <paneruvipinbusiness@gmail.com>
2022-09-29 02:03:59 -0700

    mobile uui

--- src/components/SelectedUserCard.tsx
+++ src/components/SelectedUserCard.tsx
@@ -6,7 +6,7 @@ type UserCardProps = {
 };
 
 const UserCard: FC<UserCardProps> = ({user}) => {
-  return <div className="md:h-60 rounded-md gap-x-20  shadow-md bg-white flex justify-between items-center px-20 ">
+  return <div className="md:h-60 rounded-md md:gap-x-20 gap-x-2 shadow-md bg-white flex justify-between items-center md:px-20 ">
            <img className="w-44 h-44 rounded-full shadow-md object-cover" src={(user?.picture?.medium)
            ?(user?.picture?.medium)
             :'https://www.clipartmax.com/png/middle/364-3643767_about-brent-kovacs-user-profile-placeholder.png'}></img>
@@ -17,7 +17,7 @@ const UserCard: FC<UserCardProps> = ({user}) => {
             <p>{`${user.location.city} ${user.location.state} ${user.location.country}`}</p>
             <p>{user.gender}</p>
            </div>:
-           <div className="space-y-20 ">
+           <div className="md:space-y-20 ">
            <p  className="text-red-300 text-xl ">Click on the cards below to see user related data here</p>
            <FaRegHandPointDown className=" w-20 h-20 text-yellow-500 animate-bounce"/>
            </div>
